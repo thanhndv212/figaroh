1ce3084df01d072a8e71c792a59045ac7dc12e62
thanhndv212 <dinhvinhthanhnguyen@gmail.com>
2022-03-10 23:28:54 +0100

    remove unnecessary files

--- scripts/models/others/python/example_robot_data/__init__.py
+++
@@ -1,4 +0,0 @@
-# flake8: noqa
-from .robots_loader import (getModelPath, loadANYmal, loadHyQ, loadICub, loadKinova, loadRomeo, loadSolo, loadTalos,
-                            loadTalosArm, loadTalosLegs, loadTiago, loadTiagoNoHand, loadUR, readParamsFromSrdf,
-                            loadHector, loadDoublePendulum)
